c56d64b0f94aa758d56c2e956686aaa550a6313c
Susobhan Akhuli <susobhanakhuli8537@gmail.com>
2023-08-15 03:15:59 +0530

    importing packages, use backend url and send get request instead of post using axios

--- src/app/getpdf/page.js
+++ src/app/getpdf/page.js
@@ -1,34 +1,66 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
+import axios from "axios";
+import fileDownload from "js-file-download";
 import style from "../style.module.css";
 import urlpic from "../images/url-img.png";
 import dowpic from "../images/down-logo.png";
 
 export default function GetPdf() {
   const backend_url = process.env.NEXT_PUBLIC_BACKEND_URL;
-  const [urls, setUrls] = useState({urls: ""});
+  const [url, setUrl] = useState(null);
 
   const handleUrl = (e) => {
     e.preventDefault();
-    console.log(urls);
+    console.log(url);
     console.log(backend_url);
-    // fetch(`${backend_url}/here` || "http://localhost:8000/here", {
-      fetch(`${backend_url}/here`, {
-      method: "POST",
+    axios({
+      url: `${backend_url}/download`,
+      method: "GET",
+      responseType: "blob",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `${url}`,
       },
-      body: JSON.stringify(urls),
     })
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data.message);
-        alert(data.message);
+      .then((res) => {
+        fileDownload(res.data, "WebToPdf.pdf");
       })
       .catch((err) => console.log(err));
 
-    // console.log(url);
+    // fetch("http://localhost:8000/download", {
+    //   // fetch(`${backend_url}/here`, {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `${url}`,
+    //   },
+    // })
+    //   // fetch("http://localhost:8000/here", {
+    //   //   // fetch(`${backend_url}/here`, {
+    //   //   method: "POST",
+    //   //   headers: {
+    //   //     "Content-Type": "application/json",
+    //   //   },
+    //   //   body: JSON.stringify(url),
+    //   // })
+    //   .then((res) => fileDownload(res.formData, "WebToPdf.pdf"))
+    //   // .then((res) => res.json())
+    //   // .then((data) => {
+    //   //   // console.log(data.message);
+    //   //   // alert(data.message);
+    //   //   // console.log(data.pdf);
+
+    //   //   // const blob = new Blob([data.pdf], { type: "application/pdf" });
+    //   //   // const url = window.URL.createObjectURL(blob);
+    //   //   window.open(data);
+    //   //   // window.open(data.pdf, "_blank", "location=yes,height=570,width=520,scrollbars=yes,status=yes");
+    //   //   // data.download(data.pdf);
+    //   // })
+    //   .catch((err) => console.log(err));
+
+    // // console.log(url);
   };
 
   return (
@@ -49,7 +81,7 @@ export default function GetPdf() {
             required
             className={style.url_box}
             onChange={(e) => {
-              setUrls({ urls: e.target.value });
+              setUrl(e.target.value);
             }}
           />
         </div>
